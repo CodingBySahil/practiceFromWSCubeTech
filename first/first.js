4f797ee1e0c950f1dea09ee11674c59394d0c1ea
CodingBySahil <muhammadsahil345737@gmail.com>
2024-03-15 00:04:08 -0700

    Obeject and arrays in typescript

--- first/first.js
+++ first/first.js
@@ -1,10 +1,30 @@
-var number = 3;
-console.log("number ", 3);
-var str = "ALLAH U AKBAR";
-console.log("string ".concat(str));
-var bol = true;
-console.log("boolen : ", bol);
-var arr = [1, 2, 3, 4, 5, 6];
-console.log("Numbers array : ", arr);
-var arr1 = ["a", "s", "f", "f"];
-console.log("string array : ", arr1);
+// let number: number = 3;
+// console.log("number ", 3);
+// let str: string = "ALLAH U AKBAR";
+// console.log(`string ${str}`);
+// let bol: boolean = true;
+// console.log("boolen : ", bol);
+// let arr: number[] = [1, 2, 3, 4, 5, 6];
+// console.log("Numbers array : ", arr);
+// let arr1: string[] = ["a", "s", "f", "f"];
+// console.log("string array : ", arr1);
+// learning and Playing with arrays
+// let newArray = [1, 2, 3,"hello","hiiiiiiii"];
+// newArray.push("str")
+// // newArray[3] = 2
+// // newArray[4] = 2
+// newArray[0] = "true"
+// console.log(newArray)
+// learning and playing with objects
+// let newObject:{
+//   name: string,
+//   age: number,
+//   isMarried: boolean,
+// };
+// newObject = {
+//   name: "sahil",
+//   age: 20.5,
+//   isMarried: false,
+// };
+// // newObject.age = 20.5
+// console.log(newObject)
--- first/first.ts
+++ first/first.ts
@@ -1,14 +1,37 @@
-let number: number = 3;
-console.log("number ", 3);
+// let number: number = 3;
+// console.log("number ", 3);
 
-let str: string = "ALLAH U AKBAR";
-console.log(`string ${str}`);
+// let str: string = "ALLAH U AKBAR";
+// console.log(`string ${str}`);
 
-let bol: boolean = true;
-console.log("boolen : ", bol);
+// let bol: boolean = true;
+// console.log("boolen : ", bol);
 
-let arr: number[] = [1, 2, 3, 4, 5, 6];
-console.log("Numbers array : ", arr);
+// let arr: number[] = [1, 2, 3, 4, 5, 6];
+// console.log("Numbers array : ", arr);
 
-let arr1: string[] = ["a", "s", "f", "f"];
-console.log("string array : ", arr1);
+// let arr1: string[] = ["a", "s", "f", "f"];
+// console.log("string array : ", arr1);
+
+// learning and Playing with arrays
+
+// let newArray = [1, 2, 3,"hello","hiiiiiiii"];
+// newArray.push("str")
+// // newArray[3] = 2
+// // newArray[4] = 2
+// newArray[0] = "true"
+// console.log(newArray)
+
+// learning and playing with objects
+// let newObject:{
+//   name: string,
+//   age: number,
+//   isMarried: boolean,
+// };
+// newObject = {
+//   name: "sahil",
+//   age: 20.5,
+//   isMarried: false,
+// };
+// // newObject.age = 20.5
+// console.log(newObject)
